5a2dc5efb64e8c3db2ee685ee1c9f27348b205dc
Indrajit Chakrabarty <indyfromoz@gmail.com>
2018-04-24 21:24:24 +1000

    Flutter.io Getting started codelab step 4 & 5 complete

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -8,15 +8,8 @@ class MyApp extends StatelessWidget {
   // The build method describes how to display the widget in terms of other, lower level widgets.
   Widget build(BuildContext context) { 
     return new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold( // Provides Appbar, title and body
-        appBar: new AppBar(
-          title: new Text('Welcome to Flutter'),
-        ),
-        body: new Center(
-          child: new RandomWords(),
-        ),
-      ),
+      title: 'Startup Name Generator',
+      home: new RandomWords()
     );
   }
 }
@@ -27,9 +20,64 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[]; 
+  final _saved = new Set<WordPair>();
+
+  final _bigFont = const TextStyle(fontSize: 18.0);
+  
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        // Add a one-pixel-high divider widget before each row in theListView.
+        if (i.isOdd) return new Divider();
+
+        // The syntax "i ~/ 2" divides i by 2 and returns an integer result.
+        // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+        // This calculates the actual number of word pairings in the ListView,
+        // minus the divider widgets.
+        final index = i ~/ 2;
+        // If you've reached the end of the available word pairings...
+        if (index >= _suggestions.length) {
+          // ...then generate 10 more and add them to the suggestions list.
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _bigFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
   }
 }
